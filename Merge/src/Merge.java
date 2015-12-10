bfaa7763b71c4fd7b1c7d3cbf46a218819ab20ad
Abdeselam El-Haman Abdeselam <aelhaman@gmail.com>
2015-12-10 17:53:01 +0100

    [Add] Thread execution added, just manage how to call each thread

--- Merge/src/Merge.java
+++ Merge/src/Merge.java
@@ -7,23 +7,23 @@ import java.util.logging.Logger;
  * @author Abde
  * @param <T>
  */
-public class Merge<T> implements Runnable {
+public class Merge<T extends Comparable> implements Runnable {
 
     /**
      * @param args the command line arguments
      */
     
-    private final long sizefirst;
-    private final long sizelast;
+    private final int sizefirst;
+    private final int sizelast;
     private T[] vector;
     
-    public Merge(T[] vector, final long sizel, long sizef){
+    public Merge(T[] vector, final int sizel, int sizef){
         this.sizefirst = sizef;
         this.sizelast = sizel;
         this.vector = vector;
     }
     
-    public Merge(T[] vector, final long sizel){
+    public Merge(T[] vector, final int sizel){
         this(vector,sizel,0);
     }
     
@@ -34,24 +34,35 @@ public class Merge<T> implements Runnable {
     @Override
     public void run(){
         if (sizelast - sizefirst > 1){
-            final long m = (sizelast - sizefirst)/2 + sizefirst;
+            final int m = (sizelast - sizefirst)/2 + sizefirst;
             Runnable merge1 = new Merge<>(vector,m,sizefirst);
             Runnable merge2 = new Merge<>(vector,sizelast,m);
             
-            Thread[] threads = new Thread[]{ new Thread(merge1), new Thread(merge2) };
+            Thread thread1 = new Thread(merge1);
+            Thread thread2 = new Thread(merge2);
             
-            for (Thread i : threads) i.start();
+            thread1.start();
+            thread2.start();
             
-            for (Thread i: threads){
-                try {
-                    i.join();
-                } catch (InterruptedException ex) { 
-                    Logger.getLogger(Merge.class.getName()).log(Level.SEVERE, null, ex); }
+            try {
+                thread1.join();
+                thread2.join();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Merge.class.getName()).log(Level.SEVERE, null, ex);
             }
             
-            //T[] temp = new T[(sizelast - sizefirst)/2];
-            //long i
+            
+            T[] w = (T[]) new Object[(sizelast - sizefirst)/2];
+            
+            int i =sizefirst, j=sizefirst, k=sizefirst;
+            
+            for (; i < m; ++i) w[i] = vector[i];
+            while( j < m && i < sizelast) vector[k++] = vector[i].compareTo(w[j]) < 0 ? vector[i++] : w[j++]; // overload > operator
+            while( j < m ) vector[k++] = w[j++];
+            
+            // post traitement
             
         }
     }
+    
 }
